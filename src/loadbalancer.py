3dc12d3fed19d21238f732287e15d36dd9d5df0c
Christian Berendt <stockwerk42@gmail.com>
2023-07-14 11:26:16 +0200

    Add script to fix octavia PENDING_UPDATE issues (#15)
    Part of osism/issues#589
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ src/loadbalancer.py
@@ -0,0 +1,146 @@
+import sys
+from time import sleep
+
+from loguru import logger
+import openstack
+from oslo_config import cfg
+from prompt_toolkit import prompt
+import pymysql
+
+PROJECT_NAME = "loadbalancer"
+CONF = cfg.CONF
+opts = [
+    cfg.BoolOpt("debug", help="Enable debug logging", default=False),
+    cfg.StrOpt("connection", help="Database connection string", default=None),
+    cfg.StrOpt("loadbalancer", help="Loadbalancer ID to handle", default=None),
+    cfg.StrOpt("cloud", help="Cloud name in clouds.yaml", default="service"),
+]
+CONF.register_cli_opts(opts)
+CONF(sys.argv[1:], project=PROJECT_NAME)
+
+SLEEP_WAIT_FOR_AMPHORA_BOOT = 5
+TIMEOUT_WAIT_FOR_AMPHORA_BOOT = 120
+
+if CONF.debug:
+    level = "DEBUG"
+else:
+    level = "INFO"
+
+log_fmt = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | "
+    "<level>{message}</level>"
+)
+
+logger.remove()
+logger.add(sys.stderr, format=log_fmt, level=level, colorize=True)
+
+
+def wait_for_amphora_boot(loadbalancer_id):
+    global cloud
+
+    logger.info(
+        f"Wait up to {TIMEOUT_WAIT_FOR_AMPHORA_BOOT} seconds for amphora boot of loadbalancer {loadbalancer_id}"
+    )
+
+    i = TIMEOUT_WAIT_FOR_AMPHORA_BOOT / SLEEP_WAIT_FOR_AMPHORA_BOOT
+
+    while i:
+        amphorae = cloud.load_balancer.amphorae(
+            loadbalancer_id=loadbalancer_id, status="BOOTING"
+        )
+        if not [1 for x in amphorae]:
+            break
+        i = i - 1
+        sleep(SLEEP_WAIT_FOR_AMPHORA_BOOT)
+
+
+def reset_load_balancer_provisioning_status(load_balancer):
+    global database
+
+    # MariaDB [octavia]> describe load_balancer;
+    # +---------------------+--------------+------+-----+---------+-------+
+    # | Field               | Type         | Null | Key | Default | Extra |
+    # +---------------------+--------------+------+-----+---------+-------+
+    # | project_id          | varchar(36)  | YES  |     | NULL    |       |
+    # | id                  | varchar(36)  | NO   | PRI | NULL    |       |
+    # | name                | varchar(255) | YES  |     | NULL    |       |
+    # | description         | varchar(255) | YES  |     | NULL    |       |
+    # | provisioning_status | varchar(16)  | NO   | MUL | NULL    |       |
+    # | operating_status    | varchar(16)  | NO   | MUL | NULL    |       |
+    # | enabled             | tinyint(1)   | NO   |     | NULL    |       |
+    # | topology            | varchar(36)  | YES  | MUL | NULL    |       |
+    # | server_group_id     | varchar(36)  | YES  |     | NULL    |       |
+    # | created_at          | datetime     | YES  |     | NULL    |       |
+    # | updated_at          | datetime     | YES  |     | NULL    |       |
+    # | provider            | varchar(64)  | YES  |     | NULL    |       |
+    # | flavor_id           | varchar(36)  | YES  | MUL | NULL    |       |
+    # | availability_zone   | varchar(255) | YES  | MUL | NULL    |       |
+    # +---------------------+--------------+------+-----+---------+-------+
+
+    with database.cursor() as cursor:
+        query = f"UPDATE load_balancer SET provisioning_status = 'ACTIVE' WHERE id = '{load_balancer.id}';"
+        logger.debug(query)
+        cursor.execute(query)
+        database.commit()
+
+
+# Connect to the OpenStack environment
+cloud = openstack.connect(cloud=CONF.cloud)
+
+# Connect to the database
+# NOTE: Beautify this
+connection_parts = CONF.connection.split("//")[1]
+db_username = connection_parts.split(":")[0]
+db_password = connection_parts.split(":")[1].split("@")[0]
+db_host = connection_parts.split("@")[1].split(":")[0]
+db_port = int(connection_parts.split("@")[1].split(":")[1].split("/")[0])
+db_database = connection_parts.split("@")[1].split(":")[1].split("/")[1]
+
+database = pymysql.connect(
+    host=db_host,
+    port=db_port,
+    user=db_username,
+    password=db_password,
+    database=db_database,
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+if CONF.loadbalancer:
+    load_balancer = cloud.load_balancer.get_load_balancer(CONF.loadbalancer)
+    if load_balancer.provisioning_status != "PENDING_UPDATE":
+        logger.error(
+            f"{CONF.loadbalancer} has to be in provisioning_status PENDING_UPDATE"
+        )
+        sys.exit(1)
+
+    result = prompt(f"Reset loadbalancer {CONF.loadbalancer} [yes/no]: ")
+    if result == "yes":
+        logger.info(f"Resetting {load_balancer.name}")
+        reset_load_balancer_provisioning_status(load_balancer)
+
+        logger.info(f"Triggering failover for {load_balancer.name}")
+        cloud.load_balancer.failover_load_balancer(load_balancer.id)
+        sleep(10)  # wait for the octavia API
+        wait_for_amphora_boot(load_balancer.id)
+
+else:
+
+    load_balancers = cloud.load_balancer.load_balancers(
+        provisioning_status="PENDING_UPDATE"
+    )
+    for load_balancer in load_balancers:
+        logger.info(
+            f"Loadbalancer {load_balancer.name} is in provisioning status PENDING_UPDATE"
+        )
+        result = prompt(f"Reset loadbalancer {load_balancer.name} [yes/no]: ")
+
+        if result == "yes":
+            logger.info(f"Resetting {load_balancer.name}")
+            reset_load_balancer_provisioning_status(load_balancer)
+
+            logger.info(f"Triggering failover for {load_balancer.name}")
+            cloud.load_balancer.failover_load_balancer(load_balancer.id)
+            sleep(10)  # wait for the octavia API
+            wait_for_amphora_boot(load_balancer.id)
+        else:
+            logger.debug(f"Skipping {load_balancer.name}")
